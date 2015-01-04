4b3630949e45058244f1456853676f701926fe64
cclauss <cclauss@bluewin.ch>
2015-01-04 07:50:22 +0100

    Optimize with .join()

--- zap_carriage_returns.py
+++ zap_carriage_returns.py
@@ -1,5 +1,5 @@
 filename = 'menu.py'
 with open(filename) as in_file:
-    text = [line.rstrip() + '\n' for line in in_file.readlines()]
+    text = '\n'.join([line.rstrip() for line in in_file.readlines()]) + '\n'
 with open(filename, 'w') as out_file:
     out_file.writelines(text)
