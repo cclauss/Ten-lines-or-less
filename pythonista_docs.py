6cc654e49018229eef1ffdf35386839badb35908
cclauss <cclauss@bluewin.ch>
2015-06-18 23:11:41 +0200

    Create pythonista_docs.py

---
+++ pythonista_docs.py
@@ -0,0 +1,8 @@
+import os, ui
+
+if __name__ == '__main__':
+    app_path = os.path.abspath(os.path.join(os.__file__, '../..'))
+    fmt = 'file://{}/Documentation/index.html'
+    web_view = ui.WebView(name='Pythonista Documentation')
+    web_view.load_url(fmt.format(app_path))
+    web_view.present()  # present Pythonista docs in a ui.WebView
