bac2ca97bf393bdd9ea972b79d87f9cefc518b9f
cclauss <cclauss@bluewin.ch>
2015-06-23 16:13:27 +0200

    Create pyui_variable.py

---
+++ pyui_variable.py
@@ -0,0 +1,15 @@
+# See: https://omz-forums.appspot.com/pythonista/post/5254558653612032
+# and: https://omz-forums.appspot.com/editorial/post/6075976853225472
+import json, tempfile, ui
+
+_aview = [{"class":"Button","attributes":{"font_size":64,"title":"Tap me!"}}]
+
+def load_view_from_list(view_list):
+    with tempfile.NamedTemporaryFile(suffix='.pyui') as temp_file:
+        json.dump(view_list, temp_file)
+        temp_file.seek(0)  # move the file read cursor back to byte zero
+        return ui.load_view(temp_file.name)
+
+view = load_view_from_list(_aview)
+view.action = lambda sender: sender.close()
+view.present(hide_title_bar=True)
