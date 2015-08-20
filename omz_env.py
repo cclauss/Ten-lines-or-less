88e9c5a2b14f66f07e544539557ec54f1f9500c8
cclauss <cclauss@bluewin.ch>
2015-08-20 21:29:31 +0200

    Create omz_env.py

---
+++ omz_env.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+# See: https://forum.omz-software.com/topic/2135/telling-if-i-m-pythonista-or-editorial-or-sublime-text-or-whatever
+
+omz_env = None
+try:
+    import workflow
+    omz_env = 'Editorial'
+except ImportError:
+    try:
+        import scene
+        omz_env = 'Pythonista'
+    except ImportError:
+        pass
+
+if not omz_env:
+    print('Sublime or other non-OMZ Software platform.')
+else:
+    print('Yeah!!')
