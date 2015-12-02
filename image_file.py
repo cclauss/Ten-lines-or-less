49adace4fde930fd9e6791e36616e9765b5d751f
cclauss <cclauss@bluewin.ch>
2015-12-03 00:13:48 +0100

    Update image_file.py

--- image_file.py
+++ image_file.py
@@ -2,7 +2,7 @@
 
 # See: https://forum.omz-software.com/topic/2407/images-not-loading-from-camera-roll
 
-import photos, ui
+import console, photos, ui
 
 filename = 'my_super_special_image'
 
@@ -11,7 +11,7 @@ img, metadata = photos.pick_image(include_metadata=True, raw_data=True)
 filename = metadata.get('filename', '').lower() or filename
 with open(filename, 'w') as out_file:
     out_file.write(img)
-    print(filename + ' written...')
+    console.hud_alert(filename + ' written...')
 view = ui.ImageView()
 view.image = ui.Image.named(filename)
 view.present()
