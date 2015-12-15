0a01de1a269b0f75f2ef1508a8990660361899c0
cclauss <cclauss@bluewin.ch>
2015-12-15 03:18:43 +0100

    Update pythonista_version.py

--- pythonista_version.py
+++ pythonista_version.py
@@ -1,8 +1,9 @@
 # Output: Pythonista version 1.6 (160037) on iOS 9.2 on an iPad3,4.
 # built on https://forum.omz-software.com/topic/2444/determining-pythonista-s-version/3
 
+import os, platform, plistlib, sys
+
 def pythonista_version():
-    import os, sys, plistlib
     plist = plistlib.readPlist(os.path.abspath(os.path.join(sys.executable, '../Info.plist')))
     return '{CFBundleShortVersionString} ({CFBundleVersion})'.format(**plist)
 
