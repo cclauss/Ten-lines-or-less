bafd386f5beba1b9fd67ea0642c13a20ef5f638f
cclauss <cclauss@bluewin.ch>
2015-12-18 00:33:38 +0100

    Update pythonista_docs.py

--- pythonista_docs.py
+++ pythonista_docs.py
@@ -4,6 +4,7 @@ import sys, ui
 
 if __name__ == '__main__':
     docs_path = 'file://{}/../Documentation/index.html'.format(sys.executable)
+    # webbrowser.open(docs_path)
     web_view = ui.WebView(name='Pythonista Documentation')
     web_view.load_url(docs_path)
     web_view.present()  # present Pythonista docs in a ui.WebView
