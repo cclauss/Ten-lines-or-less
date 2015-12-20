413e1c67e5c30f0aa45037d14f62a2db833e7a41
cclauss <cclauss@bluewin.ch>
2015-12-20 18:55:56 +0100

    Delivers self-documenting NotImplementedErrors

---
+++ not_implemented.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class MyClass(object):
+    def __init__(self):
+        self.my_function()
+
+    def my_function(self):
+        raise self.not_implemented()
+
+    def not_implemented(self):
+        import inspect
+        fmt = 'Class {} does not implement {}()'
+        caller_name = inspect.getouterframes(inspect.currentframe(), 2)[1][3]
+        return NotImplementedError(fmt.format(self.__class__.__name__, caller_name))
+
+MyClass()
