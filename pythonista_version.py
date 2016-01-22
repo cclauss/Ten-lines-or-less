e6dda7d821113c1a8f8757e3a62139cf25e7d654
cclauss <cclauss@bluewin.ch>
2016-01-22 17:40:55 +0100

    Added screen resolution

--- pythonista_version.py
+++ pythonista_version.py
@@ -1,12 +1,13 @@
 # Output: Pythonista version 1.6 (160037) on iOS 9.2 on an iPad3,4.
-#         Pythonista version 1.6 (160037) on iOS 9.2 on an iPad5,4.
+#         Pythonista version 2.0.1 (201000) on iOS 9.2.1 on an iPad5,4 with a screen size of 1024 x 768.
 # built on https://forum.omz-software.com/topic/2444/determining-pythonista-s-version/3
 
-import os, platform, plistlib, sys
+import os, platform, plistlib, sys, ui
 
 def pythonista_version():
     plist = plistlib.readPlist(os.path.abspath(os.path.join(sys.executable, '..', 'Info.plist')))
     return '{CFBundleShortVersionString} ({CFBundleVersion})'.format(**plist)
 
-fmt = 'Pythonista version {0} on iOS {1} on an {3}.'
-print(fmt.format(pythonista_version(), *platform.mac_ver()))
+ios_ver, _, machine_model = platform.mac_ver()
+fmt = 'Pythonista version {} on iOS {} on an {} with a screen size of {:.0f} x {:.0f}.'
+print(fmt.format(pythonista_version(), ios_ver, machine_model, *ui.get_screen_size()))
