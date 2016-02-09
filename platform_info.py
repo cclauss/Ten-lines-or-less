1ab1570816befc76f9ebb8a0416ec3b95daeea55
cclauss <cclauss@bluewin.ch>
2016-02-09 16:32:03 +0100

    simplify name formatting

--- platform_info.py
+++ platform_info.py
@@ -3,11 +3,11 @@ import inspect, platform
 for name, value in inspect.getmembers(platform):
     if name[0] != '_' and callable(value):
         try:
-            name, value = name + '()', str(value())
+            value = str(value())
         except (IndexError, TypeError):
             continue
         if value.strip("( ,')"):
-            print('{:>23} = {}'.format(name, value))
+            print('{:>23}() = {}'.format(name, value))
 
 #import sys
 #print(sys.platform, sys.version)
