f2950e3fdbb3154531163890f5d4b4b5e4e00a5f
cclauss <cclauss@bluewin.ch>
2016-02-13 23:30:05 +0100

    Create battery_infp.py

---
+++ battery_infp.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import collections, objc_util
+
+battery_info = collections.namedtuple('battery_info', 'level state')
+states = 'unknown unplugged charging full'.split()
+
+def get_battery_info():
+    device = objc_util.ObjCClass('UIDevice').currentDevice()
+    device.setBatteryMonitoringEnabled_(True)
+    try:
+        return battery_info(int(device.batteryLevel() * 100),
+                            states[device.batteryState()])
+    finally:
+        device.setBatteryMonitoringEnabled_(False)
+
+def battery_is_low(threshold = 20):
+    battery_info = get_battery_info()
+    return (battery_info.level <= threshold
+        and battery_info.state.startswith('un'))
+
+if __name__ == '__main__':
+    print(get_battery_info())  # battery_info(level=64, state='unplugged')
+    print(battery_is_low(15))  # False
