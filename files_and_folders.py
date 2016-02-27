425152f3c65b6c58065cde9ccbcebd360289ec8c
cclauss <cclauss@bluewin.ch>
2016-02-27 05:30:24 +0100

    Cut number of lines in half
    @The-Penultimate-Defenestrator use of sets is a nice optimization!
    
    # improvement liberally borrowed from:
    # https://forum.omz-software.com/topic/2784/feature-request-pythonista-built-in-file-picker

--- files_and_folders.py
+++ files_and_folders.py
@@ -1,5 +1,19 @@
 import os
+
+# improvement liberally borrowed from:
+# https://forum.omz-software.com/topic/2784/feature-request-pythonista-built-in-file-picker
+
+
 def files_and_folders(dir_path='.'):
+    '''Return a dict containing a sorted tuple of files and a sorted
+    tuple of folders'''
+    f_and_f = os.listdir(dir_path)
+    folders = [f for f in f_and_f if os.path.isdir(os.path.abspath(f))]
+    files = set(f_and_f) - set(folders)
+    return {'files': tuple(sorted(files)), 'folders': tuple(sorted(folders))}
+
+
+def old_files_and_folders(dir_path='.'):
     files = []
     folders = []
     for filename in sorted(os.listdir(dir_path)):
@@ -8,3 +22,5 @@ def files_and_folders(dir_path='.'):
         else:
             files.append(filename)
     return tuple(files), tuple(folders)
+
+print(files_and_folders())
