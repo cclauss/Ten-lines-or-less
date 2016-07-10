ec3760b1d5419682c5f940ccaabd6bd8bd834205
cclauss <cclauss@bluewin.ch>
2016-07-10 21:05:27 +0200

    Update fake_format.py

--- fake_format.py
+++ fake_format.py
@@ -18,6 +18,8 @@ print(fake_fmt('My username is {user_name} but my email address is {email}.'))
 fake = Faker()
 print(fake_fmt('{name} considers a {catch_phrase} in {country}', fake))
 
+# =====
+
 fmt = '\t{first_name:10} {last_name:10} {random_int:>7}'
 print('\n'.join(fake_fmt(fmt) for _ in range(10)))
 
