f49168f073bcb3373933c8d5b572d853abbb9891
cclauss <cclauss@bluewin.ch>
2016-07-10 20:31:03 +0200

    Update fake_format.py

--- fake_format.py
+++ fake_format.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # coding: utf-8
 
 from faker import Faker
@@ -10,7 +10,13 @@ def fake_fmt(fmt="{first_name}'s favorite number: {random_int}", fake=None):
     return fmt.format(**{field: getattr(fake, field)() for field in fields})
 
 print(fake_fmt('\nHello {name}, How is the weather in {city}, {state_abbr}?'))
+
 print(fake_fmt())
+
 print(fake_fmt('My username is {user_name} but my email address is {email}.'))
+
 fake = Faker()
-print(fake_fmt('{name} considers a {catch_phrase} in {country:5}', fake))
+print(fake_fmt('{name} considers a {catch_phrase} in {country}', fake))
+
+fmt = '\t{first_name:10} {last_name:10} {random_int:>7}'
+print('\n'.join(fake_fmt(fmt) for _ in range(10)))
