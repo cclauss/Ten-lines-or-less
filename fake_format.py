0b0ad6efc6d4273d234d261b50fcc8a4575764fd
cclauss <cclauss@bluewin.ch>
2016-07-10 20:49:20 +0200

    Added a form letter

--- fake_format.py
+++ fake_format.py
@@ -9,9 +9,9 @@ def fake_fmt(fmt="{first_name}'s favorite number: {random_int}", fake=None):
     fields = [fld.split('}')[0].split(':')[0] for fld in fmt.split('{')[1:]]
     return fmt.format(**{field: getattr(fake, field)() for field in fields})
 
-print(fake_fmt('\nHello {name}, How is the weather in {city}, {state_abbr}?'))
-
 print(fake_fmt())
+
+print(fake_fmt('Hello {name}, How is the weather in {city}, {state_abbr}?'))
 
 print(fake_fmt('My username is {user_name} but my email address is {email}.'))
 
@@ -20,3 +20,17 @@ print(fake_fmt('{name} considers a {catch_phrase} in {country}', fake))
 
 fmt = '\t{first_name:10} {last_name:10} {random_int:>7}'
 print('\n'.join(fake_fmt(fmt) for _ in range(10)))
+
+print(fake_fmt('''
+\nDear valued {credit_card_provider} customer,
+
+    On {credit_card_expire} your credit card {credit_card_number} will expire.
+Three weeks before that date, we will send a replacement card to your address:
+
+{street_address}
+{city}, {state_abbr}  {postalcode_plus4}
+
+    Yours truely,
+        {name}
+        {credit_card_provider}
+'''))
