4347e42c73771ae7e7368d04c0dbc4b1c7bbe86d
cclauss <cclauss@bluewin.ch>
2016-07-27 12:29:58 +0200

    comments

--- fake_format.py
+++ fake_format.py
@@ -9,20 +9,25 @@ def fake_fmt(fmt="{first_name}'s favorite number: {random_int}", fake=None):
     fields = [fld.split('}')[0].split(':')[0] for fld in fmt.split('{')[1:]]
     return fmt.format(**{field: getattr(fake, field)() for field in fields})
 
+# with no parameters
 print(fake_fmt())
 
+# without a fmt but no fake parameter
 print(fake_fmt('Hello {name}, How is the weather in {city}, {state_abbr}?'))
 
 print(fake_fmt('My username is {user_name} but my email address is {email}.'))
 
+# with both fmt and fake parameters
 fake = Faker()
 print(fake_fmt('{name} considers a {catch_phrase} in {country}', fake))
 
 # ===== more examples
 
+# a list of ten fake people
 fmt = '\t{first_name:10} {last_name:10} {random_int:>7}'
 print('\n'.join(fake_fmt(fmt) for _ in range(10)))
 
+# a fake form letter
 print(fake_fmt('''
 \nDear valued {credit_card_provider} customer,
 
