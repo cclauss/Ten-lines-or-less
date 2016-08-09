e9ae4639b387b2903e6b71d88f9ac6ed1881d5d0
cclauss <cclauss@bluewin.ch>
2016-08-10 00:33:33 +0200

    Create world_bank_data.py

---
+++ world_bank_data.py
@@ -0,0 +1,15 @@
+# Download and display zipped csv data from databank.worldbank.org
+# See: https://forum.omz-software.com/topic/3413/download-and-unzip-files
+
+import csv, io, requests, zipfile
+
+url = 'http://databank.worldbank.org/data/download/WDI_csv.zip'
+filename = 'WDI_Data.csv'
+
+# warning: this can take two minutes to download!!
+with zipfile.ZipFile(io.BytesIO(requests.get(url).content)) as zip_file:
+    zip_file.extractall()
+
+with open(filename, newline='') as in_file:
+    for row in csv.reader(in_file):
+        print(', '.join(row))
