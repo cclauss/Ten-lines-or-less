1ea8363a7fa97e402a2686f7257b272defe5d2f7
cclauss <cclauss@bluewin.ch>
2016-08-10 00:44:41 +0200

    Update world_bank_data.py

--- world_bank_data.py
+++ world_bank_data.py
@@ -6,10 +6,11 @@ import csv, io, requests, zipfile
 url = 'http://databank.worldbank.org/data/download/WDI_csv.zip'
 filename = 'WDI_Data.csv'
 
-# warning: this can take two minutes to download!!
+# Warning: this can take two minutes to download!!
 with zipfile.ZipFile(io.BytesIO(requests.get(url).content)) as zip_file:
     zip_file.extractall()
 
+# On Python 2, remove: ", newline=''"
 with open(filename, newline='') as in_file:
     for row in csv.reader(in_file):
         print(', '.join(row))
