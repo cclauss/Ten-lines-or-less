06df4e937ef5c91f0caac3d309a91317f9c6fe91
cclauss <cclauss@bluewin.ch>
2017-01-07 22:54:24 +0100

    Create sudoku_revisited.py

---
+++ sudoku_revisited.py
@@ -0,0 +1,13 @@
+from random import sample
+
+
+def random_board(cells=9):
+    return [sample(range(1, cells + 1), cells) for i in range(cells)]
+
+
+def board_format(board):
+    s = '\n' + '| '.join(['- '] * 9) + '\n'
+    b = s.replace('|', '-')
+    return b + s.join(' | '.join(str(y) for y in x) for x in board) + b
+
+print(board_format(random_board()))
