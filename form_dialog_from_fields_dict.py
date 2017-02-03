aa0cc7c777c6911fc41d4b3829bd4f4e9d7ab754
cclauss <cclauss@bluewin.ch>
2017-02-03 10:27:41 +0100

    plac8 flake8

--- form_dialog_from_fields_dict.py
+++ form_dialog_from_fields_dict.py
@@ -9,6 +9,7 @@ def form_dialog_from_fields_dict(title, fields_dict):
     return dialogs.form_dialog(title, [{'title': k, 'type': v}
                                for k, v in fields_dict.iteritems()])
 
+
 # left side is the field name
 # right side is the field data type
 my_fields_dict = collections.OrderedDict((
