ec6dff24e3049ddaab392f0bc5b8d8b724e41e20
cclauss <cclauss@bluewin.ch>
2017-07-08 08:57:01 +0200

    Print the trending Python repos on GitHub

---
+++ trending_python.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import bs4
+import requests
+
+url = 'https://github.com/trending?l=Python'
+soup = bs4.BeautifulSoup(requests.get(url).content, 'lxml')  # or 'html5lib'
+repos = soup.find('ol', class_="repo-list").find_all('a', href=True)
+repos = (r.text.strip().replace(' ', '') for r in repos if '/' in r.text)
+print('\n'.join(repos))
