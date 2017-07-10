8d1507bfc7468d35d8a890044feea7da709aa9e2
cclauss <cclauss@bluewin.ch>
2017-07-10 09:04:59 +0200

    Update trending_python_tester.py

--- trending_python_tester.py
+++ trending_python_tester.py
@@ -9,7 +9,7 @@ fmt = """    # {i} flake8 testing of: {long}...
     - git clone --depth=50 --branch=master https://github.com/{long} ~/{short}
     - cd ~/{short}
     # stop the build if there are Python syntax errors or undefined names
-    - flake8 . --count --select=E901,E999,F821,F822,F823 --statistics
+    - flake8 . --count --exit-zero --select=E901,E999,F821,F822,F823 --statistics
     # exit-zero treates all errors as warnings.  The GitHub editor is 127 chars wide
     - flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 """
@@ -23,4 +23,5 @@ repos = (r.text.strip().replace(' ', '') for r in repos if '/' in r.text)
 # {'i', 7, 'long': 'python/cpython', 'short': 'cpython'}
 repos = ({'i': i + 1, 'long': repo, 'short': repo.split('/')[-1]} for i, repo
          in enumerate(repos))
+print('=' * 50)
 print('\n'.join(fmt.format(**repo) for repo in repos if repo not in ignore))
