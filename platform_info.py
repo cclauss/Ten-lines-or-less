9e911d5addcf56042e25cc76d60c326f62039fe7
cclauss <cclauss@bluewin.ch>
2017-10-27 16:55:32 +0200

    Update platform_info.py

--- platform_info.py
+++ platform_info.py
@@ -12,3 +12,49 @@ for name, value in inspect.getmembers(platform):
 
 # import sys
 # print(sys.platform, sys.version)
+
+"""
+         _mac_ver_xml() = ('11.0.3', ('', '', ''), 'iPad5,4')
+                _node() = CCC-iPad
+         _sys_version() = ('CPython', '3.6.1', '', '', 'default', 'Aug 24 2017 16:20:00', 'GCC 4.2.1 Compatible Apple LLVM 8.1.0 (clang-802.0.42)')
+          _syscmd_ver() = ('', '', '')
+         architecture() = ('64bit', '')
+                 dist() = ('', '', '')
+             java_ver() = ('', '', ('', '', ''), ('', '', ''))
+             libc_ver() = ('', '')
+   linux_distribution() = ('', '', '')
+              mac_ver() = ('11.0.3', ('', '', ''), 'iPad5,4')
+              machine() = iPad5,4
+                 node() = CCC-iPad
+             platform() = Darwin-17.0.0-iPad5,4-64bit
+            processor() = 
+        python_branch() = 
+         python_build() = ('default', 'Aug 24 2017 16:20:00')
+      python_compiler() = GCC 4.2.1 Compatible Apple LLVM 8.1.0 (clang-802.0.42)
+python_implementation() = CPython
+      python_revision() = 
+       python_version() = 3.6.1
+ python_version_tuple() = ('3', '6', '1')
+              release() = 17.0.0
+               system() = Darwin
+                uname() = uname_result(system='Darwin', node='CCC-iPad', release='17.0.0', version='Darwin Kernel Version 17.0.0: Fri Sep  1 14:59:13 PDT 2017; root:xnu-4570.2.5~167/RELEASE_ARM64_T7001', machine='iPad5,4', processor='')
+              version() = Darwin Kernel Version 17.0.0: Fri Sep  1 14:59:13 PDT 2017; root:xnu-4570.2.5~167/RELEASE_ARM64_T7001
+            win32_ver() = ('', '', '', '')
+         architecture() = ('64bit', '')
+              mac_ver() = ('11.0.3', ('', '', ''), 'iPad5,4')
+              machine() = iPad5,4
+                 node() = CCC-iPad
+             platform() = Darwin-17.0.0-iPad5,4-64bit
+         python_build() = ('default', 'Aug 24 2017 16:20:00')
+      python_compiler() = GCC 4.2.1 Compatible Apple LLVM 8.1.0 (clang-802.0.42)
+python_implementation() = CPython
+       python_version() = 3.6.1
+ python_version_tuple() = ('3', '6', '1')
+              release() = 17.0.0
+               system() = Darwin
+                uname() = uname_result(system='Darwin', node='CCC-iPad', release='17.0.0', version='Darwin Kernel Version 17.0.0: Fri Sep  1 14:59:13 PDT 2017; root:xnu-4570.2.5~167/RELEASE_ARM64_T7001', machine='iPad5,4', processor='')
+              version() = Darwin Kernel Version 17.0.0: Fri Sep  1 14:59:13 PDT 2017; root:xnu-4570.2.5~167/RELEASE_ARM64_T7001
+
+ios 3.6.1 (default, Aug 24 2017, 16:20:00) 
+[GCC 4.2.1 Compatible Apple LLVM 8.1.0 (clang-802.0.42)]
+"""
