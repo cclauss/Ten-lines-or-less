a73710bf0facf8e223f988a0c631a67cee424076
cclauss <cclauss@me.com>
2019-05-20 09:55:12 +0200

    Remove undefined name

--- appex_dump.py
+++ appex_dump.py
@@ -4,15 +4,14 @@
 
 import appex, inspect  # noqa
 
-this_is_an_undefined_name
 
 def main():
     if appex.is_running_extension():
         for name_func in inspect.getmembers(appex):
             name, func = name_func
-            if name.startswith('get_'):  # find all appex.get_xxx() methods
-                print('{:<11} : {}'.format(name.partition('_')[2], func()))
+            if name.startswith("get_"):  # find all appex.get_xxx() methods
+                print("{:<11} : {}".format(name.partition("_")[2], func()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
