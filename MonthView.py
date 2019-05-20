42aade2388d5e913cbf7b0f5a564e19efb336bc8
cclauss <cclauss@me.com>
2019-05-20 10:25:04 +0200

    Fix MonthView.py default datetime.date.today()

--- MonthView.py
+++ MonthView.py
@@ -6,7 +6,7 @@ import calendar, datetime, ui  # noqa
 
 class MonthView(ui.View):
     def __init__(self, in_date=None):
-        self.name = "{0:%B %Y}".format(in_date)
+        self.name = "{0:%B %Y}".format(in_date or datetime.date.today())
         self.add_subview(ui.TextView(name="text_view", frame=(0, 0, 4096, 4096)))
         self["text_view"].editable = False
         self["text_view"].font = ("Courier", ui.get_screen_size().w / 12.25)
