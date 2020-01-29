81c6023d05960f63cdee46ce9d5f72a68d958c03
Christian Clauss <cclauss@me.com>
2020-01-29 15:27:13 +0100

    Fix URL

--- weather_where_you_are.py
+++ weather_where_you_are.py
@@ -3,7 +3,8 @@ import location, requests, speech
 loc = location.reverse_geocode(location.get_location())
 city_country = "{City},{Country}".format(**loc[0])
 print(f"Weather in {city_country}:")
-url = "http://api.openweathermap.org/data/2.5/weather?APPID=beb97c1ce62559bba4e81e28de8be095&q="
+url = ("http://api.openweathermap.org/data/2.5/weather?"
+       "APPID=beb97c1ce62559bba4e81e28de8be095&q=")
 weather = requests.get(url + city_country).json()
 for key, value in weather.items():
     print(f"{key}: {value}")
