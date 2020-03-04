8c4973927ec11e7f04628dbbd3ba6faf727c8fad
Christian Clauss <cclauss@me.com>
2020-03-05 00:44:36 +0100

    flake8 --select=E9,F63,F7,F82

--- trending_python_tester.py
+++ trending_python_tester.py
@@ -9,7 +9,7 @@ fmt = """  echo ; echo -n "{i} flake8 testing of {long} on " ; python -V
   - git clone --depth=50 --branch=master https://github.com/{long} ~/{short}
   - cd ~/{short}
   # stop the build if there are Python syntax errors or undefined names
-  - flake8 . --count --exit-zero --select=E901,E999,F821,F822,F823 --statistics
+  - flake8 . --count --exit-zero --select=E9,F63,F7,F82 --statistics
   # exit-zero treats all errors as warnings.  The GitHub editor is 127 chars wide
   - flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 """
