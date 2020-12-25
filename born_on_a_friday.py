111007daf24b8b61186db22cadeece2f7399117b
Christian Clauss <cclauss@me.com>
2020-12-25 15:11:35 +0100

    Update born_on_a_friday.py

--- born_on_a_friday.py
+++ born_on_a_friday.py
@@ -4,10 +4,10 @@ from datetime import datetime
 from typing import Tuple
 
 
-def ask_month_day_year(prompt: str = "Enter your birthday") -> Tuple[int, int, int]:
+def ask_month_day_year(prompt: str = "Enter your birthday"):
     date = input(f"{prompt} in the format: MM/DD/YYYY ")
     month, day, year = (int(x.strip()) for x in date.split("/"))
-    return tuple(month, day, year)
+    return month, day, year
 
 
 def day_of_the_week(year: int, month: int, day: int) -> str:
