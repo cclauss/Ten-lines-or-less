b1dc7fbe58a87ed9fc50b74b2f11f2b9a855ed66
Christian Clauss <cclauss@me.com>
2021-12-06 13:13:04 +0100

    Update elapsed_time.py

--- elapsed_time.py
+++ elapsed_time.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import contextlib, datetime, typing  # noqa
+from contextlib import contextmanager
+from datetime import datetime
 
 
-@contextlib.contextmanager
+@contextmanager
 def timer(name="timer"):
-    start = datetime.datetime.now()
+    start = datetime.now()
     yield
-    print("Elapsed time ({}): {}".format(name, datetime.datetime.now() - start))
+    print(f"Elapsed time ({name}): {datetime.now() - start}")
 
 
 if __name__ == "__main__":
     # measure the speed of two ways to create a dict
     with timer("dict = {}"):
-        for _ in range(100000):
-            d: typing.Dict = {}
-    with timer("dict = dict()"):  # takes almost twice as long as {}
-        for _ in range(100000):
+        for _ in range(10_000_000):
+            d: {}
+    with timer("dict = dict()"):  # takes ~50% longer than {}
+        for _ in range(10_000_000):
             d = dict()
