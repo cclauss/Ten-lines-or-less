8701a975cc033f35c54634d52912d8b827bd63fc
Amin Zayeromali <amin.zayeromali@gmail.com>
2023-01-10 00:52:05 +0330

    Correct Error on trending_python.py (#15)
    * Update trending_python.py
    
    Correct Error in python 3.9
    repos = soup.find("ol", class_="repo-list").find_all("a", href=True)
    AttributeError: 'NoneType' object has no attribute 'find_all'
    
    * Correct Bug Again
    
    Correct Bug Again

--- trending_python.py
+++ trending_python.py
@@ -5,6 +5,7 @@ import requests
 
 url = "https://github.com/trending?l=Python"
 soup = bs4.BeautifulSoup(requests.get(url).content, "lxml")  # or 'html5lib'
-repos = soup.find("ol", class_="repo-list").find_all("a", href=True)
+repos = soup.find("ol", class_="repo-list")
+repos = soup.find_all("a", href=True)
 repos = (r.text.strip().replace(" ", "") for r in repos if "/" in r.text)
 print("\n".join(repos))
