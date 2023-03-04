9c0f7e680a267156dd15e2f98d4c4a30542d000d
Christian Clauss <cclauss@me.com>
2023-03-04 16:51:39 +0100

    Ruff again

--- elapsed_time.py
+++ elapsed_time.py
@@ -22,7 +22,8 @@ def elapsed_time(name="elapsed_time"):
     2. As a context manager to time the execution of a block of code inside a function:
         with elapsed_time("my_slow_block_of_code"):
             pass
-        ."""
+    .
+    """
     start = perf_counter()
     yield
     print(f"Elapsed time ({name}): {perf_counter() - start:0.8} seconds")
