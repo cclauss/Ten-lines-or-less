672ff353b02dee2f470ba700bd993091698d1e58
stblr <68881884+JulianStiebler@users.noreply.github.com>
2025-01-17 03:50:18 +0100

    true 10line, fixed pre-commit

--- src/weather_where_you_are.py
+++ src/weather_where_you_are.py
@@ -1,10 +1,10 @@
 import geocoder
 import requests
-
+HTTP_STATUS_OK = 200
 loc = geocoder.ip('me').json # Resolves location by IP
 city_country = f"{loc['city']},{loc['country']}" # City string for URL
 APPID = "beb97c1ce62559bba4e81e28de8be095"
 weather = requests.get(f"http://api.openweathermap.org/data/2.5/weather?APPID={APPID}&q={city_country}").json()
-
-if weather.get("cod") == 200:
+if weather.get("cod") == HTTP_STATUS_OK:
     print(f"Weather in {city_country}\n------\n" + "\n".join(f"{key}: {value}" for key, value in weather.items()))
+else: print("Error fetching data.")
